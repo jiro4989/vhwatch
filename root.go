61854a7758c06fa3d01739162002bd31cc0da8b2
jiro4989 <jiroron666@gmail.com>
2019-03-21 21:58:03 +0900

    :sparkles: キー入力でプロセスを終了できるようにした

--- root.go
+++ root.go
@@ -4,7 +4,6 @@ import (
 	"os/exec"
 	"time"
 
-	"github.com/jiro4989/vhwatch/log"
 	"github.com/mattn/go-shellwords"
 	termbox "github.com/nsf/termbox-go"
 	"github.com/spf13/cobra"
@@ -22,8 +21,6 @@ var RootCommand = &cobra.Command{
 	TODO
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		log.Debug("start 'vhwatch'")
-
 		f := cmd.Flags()
 		col, err := f.GetInt("col")
 		if err != nil {
@@ -35,6 +32,7 @@ var RootCommand = &cobra.Command{
 			panic(err)
 		}
 		defer termbox.Close()
+		termbox.SetInputMode(termbox.InputEsc)
 		termbox.Flush()
 
 		// 端末の幅を取得
@@ -43,38 +41,56 @@ var RootCommand = &cobra.Command{
 		// コマンドを描画するペインを取得
 		panes := NewPanes(col, w, h, args)
 
-		// Ctrl-Cで終了されるまで無限ループ
-		for {
-			for _, p := range panes {
-				var out []byte
-				c, err := shellwords.Parse(p.Command)
-				if err != nil {
-					panic(err)
-				}
-				switch len(c) {
-				case 0:
-					// 空の文字列が渡された場合
-					return
-				case 1:
-					// コマンドのみを渡された場合
-					out, err = exec.Command(c[0]).Output()
-				default:
-					// コマンド+オプションを渡された場合
-					// オプションは可変長でexec.Commandに渡す
-					out, err = exec.Command(c[0], c[1:]...).Output()
-				}
-				if err != nil {
-					panic(err)
-				}
-				const fc = termbox.ColorDefault
-				const bc = termbox.ColorDefault
-				p.SetHeader()
-				p.SetText(out, Offset{Y: 1}, fc, bc)
-			}
-			termbox.Flush()
-			time.Sleep(1 * time.Second)
-		}
+		// 各ペイン毎にコマンドを定期実行
+		go mainloop(panes)
 
-		log.Debug("end 'vhwatch'")
+		// Ctrl-Cで終了されるまで待機
+		waitKeyInput()
 	},
+}
+
+func mainloop(panes Panes) {
+	const fc = termbox.ColorDefault
+	const bc = termbox.ColorDefault
+	for {
+		for _, p := range panes {
+			var out []byte
+			c, err := shellwords.Parse(p.Command)
+			if err != nil {
+				panic(err)
+			}
+			switch len(c) {
+			case 0:
+				// 空の文字列が渡された場合
+				return
+			case 1:
+				// コマンドのみを渡された場合
+				out, err = exec.Command(c[0]).Output()
+			default:
+				// コマンド+オプションを渡された場合
+				// オプションは可変長でexec.Commandに渡す
+				out, err = exec.Command(c[0], c[1:]...).Output()
+			}
+			if err != nil {
+				panic(err)
+			}
+			p.SetHeader()
+			p.SetText(out, Offset{Y: 1}, fc, bc)
+		}
+		termbox.Flush()
+		time.Sleep(1 * time.Second)
+		termbox.Clear(fc, bc)
+	}
+}
+
+func waitKeyInput() {
+	for {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyCtrlC, termbox.KeyCtrlD:
+				return
+			}
+		}
+	}
 }
