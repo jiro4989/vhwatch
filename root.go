d1e915a2f6e57aacd86910b9179ee4328aafc2ed
jiro4989 <jiroron666@gmail.com>
2019-03-21 21:46:28 +0900

    :construction: とりあえず実装した

--- root.go
+++ root.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"os/exec"
+	"time"
 
 	"github.com/jiro4989/vhwatch/log"
+	"github.com/mattn/go-shellwords"
+	termbox "github.com/nsf/termbox-go"
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	cobra.OnInitialize()
 	RootCommand.Flags().IntP("col", "c", 2, "column count")
-	RootCommand.Flags().StringArrayP("vertical", "V", nil, "vertical")
-	RootCommand.Flags().StringArrayP("horizontal", "H", nil, "horizontal")
 }
 
 var RootCommand = &cobra.Command{
@@ -22,105 +23,57 @@ var RootCommand = &cobra.Command{
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Debug("start 'vhwatch'")
+
 		f := cmd.Flags()
-
-		verticalCmds, err := f.GetStringArray("vertical")
+		col, err := f.GetInt("col")
 		if err != nil {
 			panic(err)
 		}
 
-		horizontalCmds, err := f.GetStringArray("horizontal")
-		if err != nil {
+		// termboxの初期化
+		if err := termbox.Init(); err != nil {
 			panic(err)
 		}
+		defer termbox.Close()
+		termbox.Flush()
 
-		log.Debug(fmt.Sprintf("command line option parameteres. "+
-			"vertical=%v, horizontal=%v",
-			verticalCmds, horizontalCmds))
+		// 端末の幅を取得
+		w, h := termbox.Size()
 
-		// // termboxの初期化
-		// if err := termbox.Init(); err != nil {
-		// 	panic(err)
-		// }
-		// defer termbox.Close()
-		// termbox.SetInputMode(termbox.InputEsc)
-		// termbox.Flush()
-		//
-		// // 端末の幅を取得
-		// w, h := termbox.Size()
-		//
-		// vcnt := len(verticalCmds)
-		// hcnt := len(horizontalCmds)
-		//
-		// for i, v := range verticalCmds {
-		// 	p := Pane{
-		// 		Name: v,
-		// 		X:    w / vcnt * i,
-		// 		Y:    h / hcnt * 0,
-		// 	}
-		// }
-		//
-		// // どちらも指定がないときだけ引数を処理
-		// if len(verticalCmds) < 1 && len(horizontalCmds) < 1 {
-		// 	return
-		// }
-		//
-		// c, err := shellwords.Parse(cmdstr)
-		// if err != nil {
-		// 	return err
-		// }
-		// switch len(c) {
-		// case 0:
-		// 	// 空の文字列が渡された場合
-		// 	return nil
-		// case 1:
-		// 	// コマンドのみを渡された場合
-		// 	err = exec.Command(c[0]).Run()
-		// default:
-		// 	// コマンド+オプションを渡された場合
-		// 	// オプションは可変長でexec.Commandに渡す
-		// 	err = exec.Command(c[0], c[1:]...).Run()
-		// }
-		// if err != nil {
-		// 	return err
-		// }
-		//
-		// p1 := Pane{
-		// 	Name:   "Pane1",
-		// 	X:      0,
-		// 	Y:      0,
-		// 	Width:  w,
-		// 	Height: h / 2,
-		// }
-		// p2 := Pane{
-		// 	Name:   "Pane2",
-		// 	X:      0,
-		// 	Y:      h / 2,
-		// 	Width:  w,
-		// 	Height: h / 2,
-		// }
-		//
-		// const fc = termbox.ColorDefault
-		// const bc = termbox.ColorDefault
-		//
-		// offset := Offset{X: 0, Y: 1}
-		// out, err := exec.Command("ls", "-l").Output()
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// p1.SetHeader()
-		// p1.SetText(out, offset, fc, bc)
-		//
-		// out, err = exec.Command("ls", "-la").Output()
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// p2.SetHeader()
-		// p2.SetText(out, offset, fc, bc)
-		//
-		// termbox.Flush()
-		//
-		// time.Sleep(5 * time.Second)
+		// コマンドを描画するペインを取得
+		panes := NewPanes(col, w, h, args)
+
+		// Ctrl-Cで終了されるまで無限ループ
+		for {
+			for _, p := range panes {
+				var out []byte
+				c, err := shellwords.Parse(p.Command)
+				if err != nil {
+					panic(err)
+				}
+				switch len(c) {
+				case 0:
+					// 空の文字列が渡された場合
+					return
+				case 1:
+					// コマンドのみを渡された場合
+					out, err = exec.Command(c[0]).Output()
+				default:
+					// コマンド+オプションを渡された場合
+					// オプションは可変長でexec.Commandに渡す
+					out, err = exec.Command(c[0], c[1:]...).Output()
+				}
+				if err != nil {
+					panic(err)
+				}
+				const fc = termbox.ColorDefault
+				const bc = termbox.ColorDefault
+				p.SetHeader()
+				p.SetText(out, Offset{Y: 1}, fc, bc)
+			}
+			termbox.Flush()
+			time.Sleep(1 * time.Second)
+		}
 
 		log.Debug("end 'vhwatch'")
 	},
