0ff2d3a4239d6afc42e3743b8b1ee31bff5573d2
jiro4989 <jiroron666@gmail.com>
2019-03-22 19:37:58 +0900

    :sparkles: テキスト整形をテストできるように関数の構成を変更した

--- pane.go
+++ pane.go
@@ -4,7 +4,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/mattn/go-runewidth"
 	termbox "github.com/nsf/termbox-go"
 )
 
@@ -69,15 +68,18 @@ func paneY(c, h, i int) int {
 // DrawHeader はヘッダ情報を描画する。
 // ヘッダはコマンド名を表示し、背景色を変更する。
 func (p *Pane) DrawHeader() {
+	const fc = termbox.ColorBlack
+	const bc = termbox.ColorWhite
+
 	// ヘッダの背景色を変更
 	w, _ := termbox.Size()
 	bgline := strings.Repeat(" ", w)
-	p.DrawLineText(0, p.Y, bgline, termbox.ColorBlack, termbox.ColorWhite, true)
+	p.DrawText(0, 0, []byte(bgline), fc, bc, true)
 
 	// 上書きでテキストをセット
 	now := time.Now().Format("2006/01/02 15:04:05")
 	line := p.Name + " " + now
-	p.DrawLineText(0, p.Y, line, termbox.ColorBlack, termbox.ColorWhite, true)
+	p.DrawText(0, 0, []byte(line), fc, bc, true)
 }
 
 // DrawText はテキストをペインにセットする。
@@ -85,50 +87,8 @@ func (p *Pane) DrawHeader() {
 // 超過しないように切り落とす。
 // termbox.Flushしないので、別途Flushが必要
 func (p *Pane) DrawText(x, y int, b []byte, fc, bc termbox.Attribute, chopLongLines bool) {
-	s := string(b)
-	lines := strings.Split(s, "\n")
-	for i, line := range lines {
-		i += y
-		if p.Height < i {
-			break
-		}
-		fy := p.Y + i
-		p.DrawLineText(x, fy, line, fc, bc, chopLongLines)
-		// 折り返しが発生していたときに、折り返された行数分ずらす
-		if !chopLongLines {
-			w := runewidth.StringWidth(line)
-			y += w / p.Width
-		}
-	}
-}
-
-// DrawLineText(y は１行のテキストをターミナルに書く。
-// １行がペインの幅に収まりきらないときに、切り詰めるか、折り返すかを
-// chopLongLinesで管理する。
-// termbox.Flushはしない。
-func (p *Pane) DrawLineText(x, y int, line string, fc, bc termbox.Attribute, chopLongLines bool) {
-	var colPos int
-	for _, c := range []rune(line) {
-		colPos += x
-		if p.Width < colPos {
-			if chopLongLines {
-				// はみ出してしまっていたときは
-				// テキストが切り落とされていることを明示する
-				termbox.SetCell(p.X+colPos-1, y, '.', fc, bc)
-				termbox.SetCell(p.X+colPos-2, y, '.', fc, bc)
-				break
-			}
-			colPos = 0
-			y++
-		}
-		fx := p.X + colPos
-		termbox.SetCell(fx, y, c, fc, bc)
-		l := runewidth.StringWidth(string(c))
-		// マルチバイト文字を処理したときは1文字ずらす
-		if 1 < l {
-			termbox.SetCell(fx+1, y, ' ', fc, bc)
-			colPos++
-		}
-		colPos++
+	prunes := NewPaneRunes(p, x, y, b, chopLongLines)
+	for _, r := range prunes {
+		termbox.SetCell(r.X, r.Y, r.Rune, fc, bc)
 	}
 }
